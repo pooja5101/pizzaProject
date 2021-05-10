fbaf580c25f62fd200449121d8303b4094d6425b
pooja5101 <47268884+pooja5101@users.noreply.github.com>
2021-05-10 13:22:11 +0530

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,39 @@
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+const pizaSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
